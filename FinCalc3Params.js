b330721f421e964a4de2a1c7f4d545715b5c4380
drppayne <56894111+drppayne@users.noreply.github.com>
2025-10-08 07:08:57 -0700

    Add files via upload

---
+++ FinCalc3Params.js
@@ -0,0 +1,16 @@
+
+
+
+  KJE.parameters.set("SIMPLE",true);
+
+
+
+/**V3_CUSTOM_CODE**/
+/* <!--
+  Financial Calculators, &copy;1998-2024 KJE Computer Solutions, Inc.
+  For more information please see:
+  <A HREF="https://www.dinkytown.net">https://www.dinkytown.net</A>
+ -->
+ */
+
+
